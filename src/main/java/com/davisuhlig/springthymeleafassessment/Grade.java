f13454b12d8f13d250689bec423af5270d9cd7dc
Davis Uhlig <duhlig@daviss-mbp.lan>
2017-08-01 16:04:46 -0400

    initial commit

---
+++ src/main/java/com/davisuhlig/springthymeleafassessment/Grade.java
@@ -0,0 +1,8 @@
+package com.davisuhlig.springthymeleafassessment;
+
+/**
+ * Created by duhlig on 8/1/17.
+ */
+public enum Grade {
+    First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Tenth, Eleventh, Twelfth
+}
---
+++ src/main/java/com/davisuhlig/springthymeleafassessment/SpringThymeleafAssessmentApplication.java
@@ -0,0 +1,12 @@
+package com.davisuhlig.springthymeleafassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafAssessmentApplication.class, args);
+	}
+}
